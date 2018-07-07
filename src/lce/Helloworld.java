827b9afec382de160e0b328c5e6e54b03a0c9453
lce <lichangen@shangquanquan.com>
2018-07-07 19:47:14 +0800

    阿森松岛

--- src/lce/Helloworld.java
+++ src/lce/Helloworld.java
@@ -5,7 +5,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("abba");
-		System.out.println("aa");
+		System.out.println("aa啊啊是第三方");
 	}
 
 }
