7a6f0ffd281c1b010a38115853763b0990e135c3
lce <lichangen@shangquanquan.com>
2018-07-07 19:53:37 +0800

    解决冲突

--- src/lce/Helloworld.java
+++ src/lce/Helloworld.java
@@ -4,8 +4,8 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("abba");
-		System.out.println("aa啊啊是第三方");
+		System.out.println("abbaertyui");
+		System.out.println("aaxxxx");
 	}
 
 }
